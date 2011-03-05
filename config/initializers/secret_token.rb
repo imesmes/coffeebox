778333a7fd058c3b00655804a93023fb61c4e98c
david <david@imesmes.com>
2011-03-05 20:37:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeebox::Application.config.secret_token = '4c6ce5844082ebfc8df952d981ac47c4363335459cbc58f69e4ce7584d33571dc3a50f8a6a3e52b938cffc472b2fafcfe36a94529f100b964704c452068dc74f'
